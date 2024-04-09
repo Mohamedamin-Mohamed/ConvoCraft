9b207902b76c16d29ddebfcaad6a04359e7f43e2
Mohamedamin Mohamed <mohamedamin204080@gmail.com>
2024-04-09 12:54:57 -0500

    Adding files

--- src/Frontend/Redux/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userSlice  from './userSlice'
-
-const store = configureStore({
-    reducer: {
-    userInfo: userSlice
-    }
-})
-export default store
